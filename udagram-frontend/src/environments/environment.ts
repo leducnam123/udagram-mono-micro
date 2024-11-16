9653e451f70c3b1bf447395bfb249ccb986a7ee8
kyriosanderson <leducnam209@gmail.com>
2024-11-16 23:20:22 +0700

    updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a484a560968e34263a4433efa3beb352-44105210.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
